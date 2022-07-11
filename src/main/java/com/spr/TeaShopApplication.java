437f3873db75f8a84c40b6c4636814b63b36ad77
Volodymyr Yeikovych <yeikovychvolodymyrwork@gmail.com>
2022-07-11 11:50:29 +0200

    First Entry.

---
+++ src/main/java/com/spr/TeaShopApplication.java
@@ -0,0 +1,13 @@
+package com.spr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spr/TeaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
